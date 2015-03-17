567c703009bbd7151d994bfa993749ec8e2e0276
l23184 <l23184@hotmail.com>
2015-03-17 13:40:58 +0100

    pasted over

---
+++ cpp/huffman.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+#include "huffread.h"
+
+int main(int argc, const char *argv[])
+{
+    if(argc != 2){
+        std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
+    }
+
+    std::vector<tree_leaf> leafs;
+    read_file(&leafs, argv[1]);
+
+    std::sort(leafs.begin(), leafs.end(), leaf_sort);
+
+    print_list(&leafs);
+
+    return 0;
+}
